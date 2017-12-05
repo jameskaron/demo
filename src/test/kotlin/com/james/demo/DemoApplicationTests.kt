1fdaa4c0194edd73e5d603b3c42406bd6c4c2ce2
jameskaron <jameskaron@126.com>
2017-12-05 19:57:37 +0800

    1

--- src/test/kotlin/com/james/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/james/demo/DemoApplicationTests.kt
@@ -17,8 +17,9 @@ class DemoApplicationTests {
 
 	@Test
 	fun test(){
-		val list = listOf(1,2,3,4,5,6)
-//		assertEquals(mapOf("odd" to listOf(1, 3, 5), "even" to listOf(2, 4, 6)), list.groupBy { if (it % 2 == 0) "even" else "odd" })
-		print(list.groupBy { if (it % 2 == 0) "even" else "odd"})
+        val unsortedList = listOf(3, 2, 7, 5)
+        assertEquals(listOf(3, 7, 2, 5), unsortedList.sortedBy { it % 3 })
+        println(2%3)
+        println(unsortedList.sortedBy { it % 3 })
 	}
 }
